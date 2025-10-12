662e69ac54aa313a3c577dc265612e76cb7f3dbc
Federico Ariel Gomez <54158552+Fede-Gomez@users.noreply.github.com>
2025-10-12 17:30:24 -0300

    Change cart button text to 'Encargar'
    Updated the purchase button label in FloatingCart from 'Comprar' to 'Encargar' to better reflect the intended action.

--- src/components/floating-cart/FloatingCart.tsx
+++ src/components/floating-cart/FloatingCart.tsx
@@ -87,7 +87,7 @@ export default function FloatingCart() {
             className="cart-purchase-button"
             onClick={handleWhatsAppPurchase}
           >
-            Comprar
+            Encargar
           </button>
         )}
       </div>
